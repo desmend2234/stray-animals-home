7cc7873fd485bc83eead170fc7e9ab0f26a67418
李季興 <jamesli@lijixingdeMacBook-Pro.local>
2024-08-08 20:53:45 +0800

    fix

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,57 +1,57 @@
-// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-// import { createUploadthing, type FileRouter } from 'uploadthing/next';
-// import { UploadThingError } from 'uploadthing/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+import { UploadThingError } from 'uploadthing/server';
 
-// const f = createUploadthing();
+const f = createUploadthing();
 
-// // FileRouter for your app, can contain multiple FileRoutes
-// export const ourFileRouter = {
-//   // Define as many FileRoutes as you like, each with a unique routeSlug
-//   imageUploader: f({ image: { maxFileSize: '4MB', maxFileCount: 5 } })
-//     // Set permissions and file types for this FileRoute
-//     .middleware(async ({ req }) => {
-//       // This code runs on your server before upload
-//       const { getUser } = getKindeServerSession();
-//       const user = await getUser();
-//       // If you throw, the user will not be able to upload
-//       if (!user) throw new UploadThingError('Unauthorized');
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({ image: { maxFileSize: '4MB', maxFileCount: 5 } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError('Unauthorized');
 
-//       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-//       return { userId: user.id };
-//     })
-//     .onUploadComplete(async ({ metadata, file }) => {
-//       // This code RUNS ON YOUR SERVER after upload
-//       console.log('Upload complete for userId:', metadata.userId);
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log('Upload complete for userId:', metadata.userId);
 
-//       console.log('file url', file.url);
+      console.log('file url', file.url);
 
-//       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-//       return { uploadedBy: metadata.userId };
-//     }),
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
 
-//   fileUploader: f({
-//     'application/zip': { maxFileCount: 1, maxFileSize: '4MB' },
-//   })
-//     // Set permissions and file types for this FileRoute
-//     .middleware(async ({ req }) => {
-//       // This code runs on your server before upload
-//       const { getUser } = getKindeServerSession();
-//       const user = await getUser();
-//       // If you throw, the user will not be able to upload
-//       if (!user) throw new UploadThingError('Unauthorized');
+  fileUploader: f({
+    'application/zip': { maxFileCount: 1, maxFileSize: '4MB' },
+  })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError('Unauthorized');
 
-//       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-//       return { userId: user.id };
-//     })
-//     .onUploadComplete(async ({ metadata, file }) => {
-//       // This code RUNS ON YOUR SERVER after upload
-//       console.log('Upload complete for userId:', metadata.userId);
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log('Upload complete for userId:', metadata.userId);
 
-//       console.log('file url', file.url);
+      console.log('file url', file.url);
 
-//       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-//       return { uploadedBy: metadata.userId };
-//     }),
-// } satisfies FileRouter;
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
 
-// export type OurFileRouter = typeof ourFileRouter;
+export type OurFileRouter = typeof ourFileRouter;
